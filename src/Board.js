13617903ef5040c58724a9761527c40ec7155423
Wachirawit-s63 <s6301012630150@email.kmutnb.ac.th>
2022-02-21 16:54:29 +0700

    add N for size of table OX and calculateWinner but didn't fisnish

--- src/Board.js
+++ src/Board.js
@@ -1,43 +1,47 @@
 import Square from "./Square";
 import React from "react";
-import InN from "./InN";
 
 class Board extends React.Component {
 
-  constructor(props) {
-    super(props);
-      
-    };
-                    
+  // constructor(props) {
+  //   super(props);
+  //   this.state= {value: 5}
+  //   // this.setState({ value : props.N});
+  //   console.log(props.N)
+  //   console.log(this.state.value)
+  //   // console.log(this.setState.value)
+  // }
+  
   renderSquare(i) {
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        size={this.props.size}
       />
     );
   }
+  
+  createBoard(N){
+  let area_board = []
+  let c = 0;
+  for (let a = 0; a < N; a++ ){
+    let row_board = []
+    for (let b = 0; b < N; b++ ){
+      row_board.push(this.renderSquare(c))
+      c +=1
+    }
+    area_board.push(<div className="board-row">{row_board}</div>)
+  }
+  return area_board
+}
 
   render() {  
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {this.createBoard(this.props.size)}
         </div>
-      );
+      )
   }
 }
 
--- src/Game.js
+++ src/Game.js
@@ -1,33 +1,49 @@
 import Board from "./Board"
 import React from "react";
-import InN from "./InN"
 
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+  function calculateWinner(squares, dimension){
+    const rows = new Array(dimension).fill(0);
+    const cols = new Array(dimension).fill(0);
+    const diag = new Array(2).fill(0);
+    for (let row = 0; row < dimension; row++) {
+      for (let col = 0; col < dimension; col++) {
+        const square = squares[row * dimension + col];
+        if (square === "X") {
+          rows[row]++;
+          cols[col]++;
+        }
+        else if (square === "O") {
+          rows[row]--;
+          cols[col]--;
+        }
+        if (row === col) 
+          diag[0] += square === "X" ? 1 : -1;
+        if (row === dimension - col - 1) 
+          diag[1] += square === "X" ? 1 : -1;
       }
     }
+    for (let i = 0; i < dimension; i++) {
+      if (rows[i] === dimension || cols[i] === dimension) 
+          return "X";
+      else if (rows[i] === -dimension || cols[i] === -dimension) 
+          return "O";
+    }
+    if (diag[0] === dimension || diag[1] === dimension) 
+      return "X";
+    
     return null;
-}
-
+  }
 
 class Game extends React.Component {
-
+  
     constructor(props) {
         super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClickButton = this.handleClickButton.bind(this);
+        this.handleClickButton2 = this.handleClickButton2.bind(this);
         this.state = {
+          size: 3,
+          changesize :true,
           history: [{
             squares: Array(9).fill(null),
           }],
@@ -35,6 +51,15 @@ class Game extends React.Component {
           xIsNext: true,
         };
     }
+
+    handleChange(event) {
+      if (this.state.changesize === true) {
+      this.setState({size:event.target.value ,
+      history: [{
+        squares: Array((event.target.value)*(event.target.value)).fill(null)
+      }] 
+    })
+  }}
 
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
@@ -53,6 +78,19 @@ class Game extends React.Component {
         });
     }
 
+    handleClickButton(){
+      this.setState({ 
+        changesize : false })
+      alert("Size set to "+  this.state.size )
+    }
+
+    handleClickButton2(){
+      this.setState({ 
+        changesize : true ,
+        })
+      alert("You can change size" )
+    }
+
     jumpTo(step) {
       this.setState({
         stepNumber: step,
@@ -60,11 +98,14 @@ class Game extends React.Component {
       });
     }
 
+    handleSubmit(event) {
+      event.preventDefault();
+    }
+
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
+        const winner = calculateWinner(this.state.size,current.squares,parseInt(this.state.size));
         const moves = history.map((step, move) => {
           const desc = move ?
             'Go to move #' + move :
@@ -77,26 +118,38 @@ class Game extends React.Component {
         });
 
         let status;
-        if (winner) {
-        status = 'Winner: ' + winner;
+        if (winner== "X") {
+          status = "winner: " + winner;
+        }  else if (winner == "O"){
+          status = "winner: " + winner;
         } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+          status = "Next player: " + (this.state.xIsNext ? "X" : "O");
         }
-
+        
         return (
+          <div>
+          <form onSubmit={this.handleSubmit} >
+          <label>
+            Input N for size of table (N*N) (at least 3) :
+            <input type="number" onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Lock size" onClick={this.handleClickButton}/>
+          <input type="submit" value="Unlock size" onClick={this.handleClickButton2}/>
+        </form>
             <div className="game">
               <div className="game-board">
                 <Board
                   squares={current.squares}
                   onClick={(i) => this.handleClick(i)}
+                  size={this.state.size}
                 />
               </div>
               <div className="game-info">
                 <div>{status}</div>
                 <ol>{moves}</ol>
               </div>
-              <InN/>
             </div>
+          </div>
         );
     }
   }
